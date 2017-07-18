eb9653f37f3eda28b42c9a6553c82ad4f031930b
Robert O'Connell home <roconnell.galway@gmail.com>
2017-07-18 21:05:56 +0100

    First WPF page created replacing About WinForm

---
+++ IESandDACadmt/View/WpfAbout.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IESandDACadmt.View
+{
+    /// <summary>
+    /// Interaction logic for WpfAbout.xaml
+    /// </summary>
+    public partial class WpfAbout : Window
+    {
+        public WpfAbout()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
